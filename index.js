2284b304f4230b603e7fdea40533fd7526dd985f
Marc Schröder <45717371+Marc5214@users.noreply.github.com>
2021-09-28 20:31:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -93,6 +93,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8080, '127.0.0.1', () => {
+server.listen(3080, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
